d87cac941bfbf4e42144bf2de1916894a5f16516
Gyeyoung, Jung <gye0203@gmail.com>
2023-04-04 20:14:34 +0900

    배터리 아이콘 추가

--- app/src/main/java/com/cnuhci/nightlynudge/service/ForegroundService.java
+++ app/src/main/java/com/cnuhci/nightlynudge/service/ForegroundService.java
@@ -60,7 +60,7 @@ public class ForegroundService extends Service {
         }
 
         Notification notification = new NotificationCompat.Builder(getApplicationContext(), FOREGROUND_SERVICE_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setSmallIcon(R.drawable.bat_100)
                 .setContentText("App is Running: " + date )
                 .setContentTitle("NightlyNudge")
                 .setChannelId(FOREGROUND_SERVICE_CHANNEL_ID)
