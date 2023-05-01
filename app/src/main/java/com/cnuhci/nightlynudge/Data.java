7f5a0eee2dbc8547bb322e02fa7fc865781bae4f
Gyeyoung, Jung <gye0203@gmail.com>
2023-05-01 13:22:20 +0900

    notification 수정

--- app/src/main/java/com/cnuhci/nightlynudge/Data.java
+++ app/src/main/java/com/cnuhci/nightlynudge/Data.java
@@ -9,7 +9,7 @@ public class Data {
     public static String BATTERY_PERCENTAGE = "";   // 배터리 잔량
     public static String BEDTIME_USAGE_TIME = "";   // 취침 시각 ~ 기상 시각 휴대폰 사용 시간 (초)
     public static String TOTAL_USAGE_TIME = "";     // 취침 준비 시각 ~ 기상 시각 휴대폰 사용 시간 (초)
-    public static String WINDOW_ON = "";            // 휴대폰 사용 여부 (스크린 켜져있는 경우: on)
+    public static String WINDOW_ON = "none";            // 휴대폰 사용 여부 (스크린 켜져있는 경우: on)
     public static String ALARM = "none";            // 알람 준 여부 (1: 알람, 0: none)
     public static String ACTION = "none";           // 활동 시간대 여부
 }
--- app/src/main/java/com/cnuhci/nightlynudge/service/ForegroundService.java
+++ app/src/main/java/com/cnuhci/nightlynudge/service/ForegroundService.java
@@ -40,11 +40,14 @@ public class ForegroundService extends Service {
 
     private BackgroundTask task;
 
+    public static int C = 100; // 배터리 최대 용량으로, 상태 변화 시, 가변적으로 변화
+    public static float T = 0;
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         timeData = getSharedPreferences("timeData", MODE_PRIVATE);
-        notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         task = new BackgroundTask();
         updateNotification("green", 100);
 
@@ -61,11 +64,12 @@ public class ForegroundService extends Service {
 
     /**
      * Foreground service 의 내용 갱신을 위한 함수
-     * @param status        배터리의 상태 정보
-     * @param capacity      배터리의 용량 정보
+     *
+     * @param status   배터리의 상태 정보
+     * @param capacity 배터리의 용량 정보
      */
-    private void updateNotification(String status, int capacity){
-        if(notificationManager.getNotificationChannel(FOREGROUND_SERVICE_CHANNEL_ID) == null){
+    private void updateNotification(String status, int capacity) {
+        if (notificationManager.getNotificationChannel(FOREGROUND_SERVICE_CHANNEL_ID) == null) {
             createNotificationChannel(FOREGROUND_SERVICE_CHANNEL_ID);
         }
 
@@ -75,8 +79,7 @@ public class ForegroundService extends Service {
                 getApplicationContext().getPackageName());
 
 
-
-        Bitmap iconBitmap = BitmapFactory.decodeResource(getResources(),iconimage);
+        Bitmap iconBitmap = BitmapFactory.decodeResource(getResources(), iconimage);
 
         Intent intent = new Intent(this, SleepSettingActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivities(this,
@@ -86,7 +89,7 @@ public class ForegroundService extends Service {
                 .setSmallIcon(iconimage)
                 .setLargeIcon(iconBitmap)
                 .setContentText(getBattStatusText(status, capacity))
-                .setContentTitle("NightlyNudge")
+                .setContentTitle("예상 수면 및 상태")
                 .setContentIntent(pendingIntent)
                 .setChannelId(FOREGROUND_SERVICE_CHANNEL_ID)
                 .setPriority(NotificationCompat.PRIORITY_MAX)
@@ -101,14 +104,14 @@ public class ForegroundService extends Service {
     /**
      * 배터리의 정보에 변화(상태 또는 용량)가 있을 경우 알림 발생
      */
-    private void notifyBatteryChange(@Nullable String _message){
-        if(notificationManager.getNotificationChannel(STATUS_NOTIFY_CHANNEL_ID) == null){
+    private void notifyBatteryChange(@Nullable String _message) {
+        if (notificationManager.getNotificationChannel(STATUS_NOTIFY_CHANNEL_ID) == null) {
             createNotificationChannel(STATUS_NOTIFY_CHANNEL_ID);
         }
 
         String message = "배터리에 변화가 있습니다.";
 
-        if(_message != null){
+        if (_message != null) {
             message = _message;
         }
 
@@ -123,14 +126,14 @@ public class ForegroundService extends Service {
         notificationManager.notify(2, notification);
     }
 
-    private void notifyBatteryInitiation(@Nullable String _message){
-        if(notificationManager.getNotificationChannel(INITIATION_NOTIFY_CHANNEL_ID) == null){
+    private void notifyBatteryInitiation(@Nullable String _message) {
+        if (notificationManager.getNotificationChannel(INITIATION_NOTIFY_CHANNEL_ID) == null) {
             createNotificationChannel(INITIATION_NOTIFY_CHANNEL_ID);
         }
 
         String message = "배터리가 활성화 됩니다.";
 
-        if(_message != null){
+        if (_message != null) {
             message = _message;
         }
 
@@ -148,12 +151,13 @@ public class ForegroundService extends Service {
 
     /**
      * Notification channel 생성 함수
+     *
      * @param notificationChannelId 생성하려는 chennel의 고유 id 값
      */
-    private void createNotificationChannel(String notificationChannelId){
+    private void createNotificationChannel(String notificationChannelId) {
 
 
-        notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationChannel notificationChannel = new NotificationChannel(
                 notificationChannelId, notificationChannelId,
                 NotificationManager.IMPORTANCE_HIGH);
@@ -163,12 +167,13 @@ public class ForegroundService extends Service {
 
     /**
      * 배터리 상태 정보 mapping하여 foreground service에 적용될 text
-     * @param status    상태 정보
-     * @param capacity  용량 정보
-     * @return          매핑 결과
+     *
+     * @param status   상태 정보
+     * @param capacity 용량 정보
+     * @return 매핑 결과
      */
-    private String getBattStatusText(String status, int capacity){
-        String text = "수면 퀄리티: ";
+    private String getBattStatusText(String status, int capacity) {
+        String text = "";
         switch (status) {
             case "green":
                 text += "좋음 /";
@@ -184,13 +189,14 @@ public class ForegroundService extends Service {
                 break;
         }
 
-        return text + "체력: " + capacity + "%";
+        return text + " " + capacity + "%";
     }
 
 
     /**
      * 현재 시간 정보에 대해 계산
-     * @return  현재 시간 정보
+     *
+     * @return 현재 시간 정보
      */
     public static long getCurrentDateTime() {
         Date today = new Date();
@@ -210,7 +216,7 @@ public class ForegroundService extends Service {
             long currentTime = currentDate.getTime();
             Log.d("TEST", "current time :" + currentTime);
             return currentTime;
-        }catch (ParseException e){
+        } catch (ParseException e) {
         }
 
         return -1l;
@@ -222,18 +228,19 @@ public class ForegroundService extends Service {
         return null;
     }
 
-    class BackgroundTask extends AsyncTask<Integer, String, Integer>{
+    class BackgroundTask extends AsyncTask<Integer, String, Integer> {
 
         long mySleepTimer = timeData.getLong("totalSleepTime", -1l);
         long currentDiff = -1l;             // 현재 시간과 기상 시간의 차이를 봄
 
-        long totalUsageTime =  -1l;         // 수면 준비 시간과 취침 시간에 휴대폰 사용량
+        long totalUsageTime = -1l;         // 수면 준비 시간과 취침 시간에 휴대폰 사용량
         long usageTimeInSleepTime = -1l;    // 취침 시간에서의 휴대폰 사용량
 
         boolean isInitiated = false;
+        boolean isStateChanged = false;
 
-//        long totalUsageTime =  30l * 60 + 45; //test
-//        long usageTimeInSleepTime = 0l * 60; // test
+//        long totalUsageTime =  30l * 60 + 43; //test
+//        long usageTimeInSleepTime = 30l * 60; // test
 
         @Override
         protected Integer doInBackground(Integer... integers) {
@@ -244,6 +251,7 @@ public class ForegroundService extends Service {
             int prevCapacity = 100;         // 이전 수면의 잔량 기록
 
             while (isCancelled() == false) {
+                // TODO: 수면 취침 준비 시간 도입 시, 알림 추가 ==> "취침 준비 시간입니다."
                 try {
                     // check 초기화
                     if (!timeData.getBoolean("isSet", true)) {
@@ -282,20 +290,20 @@ public class ForegroundService extends Service {
                         Log.d("TEST", "현재는 수면 준비 시간입니다. 전체 사용 시간: " + totalUsageTime
                                 + " | 수면 시간에 사용 시간: " + usageTimeInSleepTime
                                 + " | 상태: " + prevStatus
-                                + " | 용량: " + prevCapacity) ;
+                                + " | 용량: " + prevCapacity);
 
                         Data.ACTION = "preparation";
 
-                        if(!isInitiated){
+                        if (!isInitiated) {
                             // 수면 배터리 활성화 시작 알림
                             Data.ALARM = "activated";
-                            notifyBatteryInitiation(null);
+                            notifyBatteryInitiation("취침 준비 시간입니다.");
                             isInitiated = true;
                         }
 
-                        if(!prevStatus.equals(checkBattStatus(totalUsageTime))){
+                        if (!prevStatus.equals(checkBattStatus(totalUsageTime))) {
                             Data.ALARM = "change";
-                            notifyBatteryChange(null);
+                            notifyBatteryChange("당신의 수면 품질이 감소하였습니다.");
                         }
                         prevStatus = checkBattStatus(totalUsageTime);
                         updateNotification(prevStatus, 100);
@@ -303,26 +311,47 @@ public class ForegroundService extends Service {
                     } else if (currentDiff <= totalSleepTime) {
 
                         // 사용자의 시간 변동에 의하여 활성화 알림이 없는 경우, 알림
-                        if(!isInitiated){
+                        if (!isInitiated) {
                             Data.ALARM = "activated";
                             notifyBatteryInitiation(null);
                             isInitiated = true;
                         }
 
                         if (checkDisplayOn()) {
+                            Data.WINDOW_ON = "on";
                             usageTimeInSleepTime += 1;
                         }
 
-                        if(!prevStatus.equals(checkBattStatus(totalUsageTime))
+                        if (!prevStatus.equals(checkBattStatus(totalUsageTime))
                                 || prevCapacity != calcBattCapacity(usageTimeInSleepTime * 1000,
-                                totalSleepTime, prevStatus)){
-                            Data.ALARM = "change";
-                            notifyBatteryChange(null);
+                                totalSleepTime, prevStatus, isStateChanged, prevCapacity)) {
+
+                            // 배터리가 5% 다를때 마다 또는 배터리 상태가 변활할 때마다 알림
+                            if (calcBattCapacity(usageTimeInSleepTime * 1000,
+                                    totalSleepTime, prevStatus, isStateChanged, prevCapacity) % 5 == 0
+                                    || !prevStatus.equals(checkBattStatus(totalUsageTime))) {
+                                Data.ALARM = "change";
+
+                                if(!prevStatus.equals(checkBattStatus(totalUsageTime)))
+                                {
+                                    notifyBatteryChange("수면의 품질이 감소하였습니다.");
+                                }
+                                if(calcBattCapacity(usageTimeInSleepTime * 1000,
+                                        totalSleepTime, prevStatus, isStateChanged, prevCapacity) % 5 == 0){
+                                    notifyBatteryChange("당신의 예상 스태미나는 " + calcBattCapacity(usageTimeInSleepTime * 1000, +
+                                                                                    totalSleepTime, prevStatus, isStateChanged, prevCapacity) % 5
+                                            + "% 입니다.");
+                                }
+                            }
+                            // 배터리의 상태가 변화될 때 새로운 그래프 연산을 위해 부울 변수 변화
+                            if(!prevStatus.equals(checkBattStatus(totalUsageTime))){
+                                isStateChanged = true;
+                            }
                         }
 
                         prevStatus = checkBattStatus(totalUsageTime);
-                        prevCapacity =   calcBattCapacity(usageTimeInSleepTime * 1000,
-                                totalSleepTime, prevStatus);
+                        prevCapacity = calcBattCapacity(usageTimeInSleepTime * 1000,
+                                totalSleepTime, prevStatus, isStateChanged, prevCapacity);
 
                         Log.d("TEST", "현재는 수면 시간입니다. 전체 사용 시간: " + totalUsageTime
                                 + " | 수면 시간에 사용 시간: " + usageTimeInSleepTime
@@ -333,7 +362,7 @@ public class ForegroundService extends Service {
                         updateNotification(prevStatus, prevCapacity);
 
                     } else {
-                        if(isInitiated){
+                        if (isInitiated) {
                             isInitiated = false;
                         }
 
@@ -343,6 +372,7 @@ public class ForegroundService extends Service {
                                 + " | 용량: " + prevCapacity);
                         Data.ACTION = "awake";
 
+                        C = 100;
                         updateNotification(prevStatus, prevCapacity);
                     }
 
@@ -350,6 +380,7 @@ public class ForegroundService extends Service {
                         mySleepTimer -= 1;
                     }
 
+                    isStateChanged = false;
                     saveCSV(totalUsageTime, usageTimeInSleepTime, prevStatus, prevCapacity);
                     Thread.sleep(1000);
                 } catch (InterruptedException ex) {
@@ -358,21 +389,22 @@ public class ForegroundService extends Service {
             return 0;
         }
 
-        private boolean saveCSV(long _totalUsageTime, long _usageTimeInSleepTime, String _status, int _capacity){
-            Data.SLEEP_TIME = timeData.getInt("mySleepHour", -1) + ":"+timeData.getInt("mySleepMin", -1);
+
+        private boolean saveCSV(long _totalUsageTime, long _usageTimeInSleepTime, String _status, int _capacity) {
+            Data.SLEEP_TIME = timeData.getInt("mySleepHour", -1) + ":" + timeData.getInt("mySleepMin", -1);
             Data.WAKE_TIME = timeData.getInt("myWakeHour", -1) + ":" + timeData.getInt("myWakeMin", -1);
             Data.TOTAL_USAGE_TIME = _totalUsageTime + "";
             Data.BEDTIME_USAGE_TIME = _usageTimeInSleepTime + "";
             Data.BATTERY_STATUS = _status;
             Data.BATTERY_PERCENTAGE = _capacity + "";
-            Data.WINDOW_ON = "none";
             CSV.writeCSV(null);
             changeCSVDefault();
 
             return true;
         }
 
-        private boolean changeCSVDefault(){
+
+        private boolean changeCSVDefault() {
             Data.TIMESTAMP = "";
             Data.SLEEP_TIME = "";
             Data.WAKE_TIME = "";
@@ -381,7 +413,7 @@ public class ForegroundService extends Service {
             Data.BEDTIME_USAGE_TIME = "";
             Data.TOTAL_USAGE_TIME = "";
             Data.WINDOW_ON = "";
-            Data.ALARM = "none";
+            Data.ALARM = "off";
             Data.ACTION = "none";
 
             return true;
@@ -390,12 +422,13 @@ public class ForegroundService extends Service {
 
         /**
          * 스마트폰 화면이 켜져있는 것 판별
+         *
          * @return 화면 켜져있는 경우 True
          */
-        private boolean checkDisplayOn(){
+        private boolean checkDisplayOn() {
             PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
-            boolean result= Build.VERSION.SDK_INT>= Build.VERSION_CODES.KITKAT_WATCH&&powerManager.isInteractive()
-                    || Build.VERSION.SDK_INT< Build.VERSION_CODES.KITKAT_WATCH&&powerManager.isScreenOn();
+            boolean result = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH && powerManager.isInteractive()
+                    || Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT_WATCH && powerManager.isScreenOn();
             return result;
         }
 
@@ -409,40 +442,41 @@ public class ForegroundService extends Service {
          * @param _usageTime 스마트폰 사용 시간 (초 * 1000)
          * @return 배터리의 상태 정보, string값
          */
-        private String checkBattStatus(long _usageTime){
+        private String checkBattStatus(long _usageTime) {
             long usage_min = _usageTime / 60;
 
-            if(0 <= usage_min && usage_min <= 30){
+            if (0 <= usage_min && usage_min <= 30) {
                 return "green";
-            } else if ( 30 < usage_min && usage_min <= 40){
+            } else if (30 < usage_min && usage_min <= 40) {
                 return "yellow";
-            } else if ( 40 < usage_min && usage_min <= 50){
+            } else if (40 < usage_min && usage_min <= 50) {
                 return "orange";
-            } else{
+            } else {
                 return "red";
             }
         }
 
         /**
          * 남은 배터리의 잔량을 계산하는 함수
-         * @param _usageTimeInSleepTime         취침 시간에 휴대폰 사용한 시간
-         * @param _totalSleepTime    사용자 지정 전체 수면 시간
-         * @param _status            배터리의 성능
-         * @return                  배터리의 잔량
+         *
+         * @param _usageTimeInSleepTime 취침 시간에 휴대폰 사용한 시간
+         * @param _totalSleepTime       사용자 지정 전체 수면 시간
+         * @param _status               배터리의 성능
+         * @return 배터리의 잔량
          */
-        private int calcBattCapacity(long _usageTimeInSleepTime, long _totalSleepTime, String _status){
+        private int calcBattCapacity(long _usageTimeInSleepTime, long _totalSleepTime, String _status, boolean _isStatusChanged, int _prevCapacity) {
             // 기울기 구하기
             // 기울기 = 100 / 전체 수면 시간 (소수)
             long h = _totalSleepTime / (60 * 60 * 1000) % 24;
             long m = _totalSleepTime / (60 * 1000) % 60;
             long s = _totalSleepTime / 1000 % 60;
-            float slop = 100 /  (h + ((float)m / 60) + ((float)s / (60*60)));
+            float slop = 100 / (h + ((float) m / 60) + ((float) s / (60 * 60)));
 
             // 수면 시간에 사용한 시간에 대해 계산
             long u_h = _usageTimeInSleepTime / (60 * 60 * 1000) % 24;
             long u_m = _usageTimeInSleepTime / (60 * 1000) % 60;
             long u_s = _usageTimeInSleepTime / 1000 % 60;
-            float u_t =  u_h + ((float)u_m / 60) + ((float)u_s / (60*60));
+            float u_t = u_h + ((float) u_m / 60) + ((float) u_s / (60 * 60));
 
             float grad = 1; // 수면의 상태에 따른 가중치
 
@@ -458,15 +492,39 @@ public class ForegroundService extends Service {
                     break;
                 default:
                     grad = 1.0F;
+                    C = 100;
+                    T = 0;
             }
 
-            int returnVal =  (int)(Math.ceil(100 - grad * slop * u_t));
+            if(_isStatusChanged){
+                // 상태 변화가 일어났기 때문에 그래프 수정이 일어난다
+                C = (int)(_prevCapacity);
+                T = u_t;
+                Log.d("TEST", "상태 변화가 일어났습니다. 용량: " + C + "| 당시 시간: " + T);
+            }
 
-            if(returnVal < 0){
+//            int returnVal = (int) (Math.ceil(capacity - grad * slop * u_t));
+            int returnVal = (int) exp(grad * slop, u_t, T, C);
+
+            if (returnVal < 0) {
                 returnVal = 0;
+            }else if (returnVal > 100){
+                returnVal = 100;
             }
 
             return returnVal;
+        }
+
+        /**
+         * 용량 계산을 위한 수식
+         * @param slop  기울기
+         * @param x     사용 시간 (취침 시간 내)
+         * @param t     정해진 좌표 값 (c 잔량일 때 당시 t값)
+         * @param c     정해진 좌표 값 (상태 변화 시 c의 값)
+         * @return
+         */
+        private int exp(float slop,float x, float t, float c){
+            return (int)(Math.ceil(-1 * slop * (x - t) + c));
         }
 
         @Override
